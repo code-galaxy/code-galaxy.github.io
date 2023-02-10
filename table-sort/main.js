acfcfe94ca5d4dc8c9a0d09a1ed57b35b0d07c9c
YAROSLAV <ykarpash@gmail.com>
2023-02-10 08:16:11 +0200

    Update main.js

--- table-sort/main.js
+++ table-sort/main.js
@@ -20,7 +20,7 @@ $(".sup_sport").click(function(){
 });
 $(".sup_esport").click(function(){
     $("[class*=sport_f]").hide();
-    $("[class*=sup_sport]").hide
+    $("[class*=sup_sport]").hid
     });
 
 
